50fa12cf1e4163e9f8b7dc689a8d6d17e42abace
Elcan Hasanli <elcan@Elcans-MacBook-Air.local>
2026-05-21 21:22:40 +0400

    last

--- lib/api.ts
+++ lib/api.ts
@@ -8,7 +8,8 @@ import type {
   User,
 } from './types';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'https://api.suman.khamsacraft.az/api';
 
 class ApiError extends Error {
   status: number;
